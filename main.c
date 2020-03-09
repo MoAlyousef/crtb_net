a3fcbd1ebea6cbd4261dba17bbf1483504eec4d6
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-10 02:12:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -17,21 +17,21 @@ int main() {
 }
 
 void hello_handler(const rtb_request *req, rtb_response *resp, void *args) {
-  rtb_request_placeholder placeholders[1];
-  rtb_request_get_placeholders(req, placeholders, 1);
   int index;
   int i;
+  rtb_request_placeholder placeholders[1];
+  rtb_request_get_placeholders(req, placeholders, 1);
   for (i = 0; i < 1; i++) {
     if (strcmp(placeholders[i].key, "name") == 0) // just to show it's queryable by key
       index = i;
   }
   const char *name = placeholders[index].value;
-  char res[200];
-  snprintf(res, 200,
+  char content[200];
+  snprintf(content, 200,
            "<html>"
            "<head><title>Hello</title></head>"
            "<body><h1>Hello %s</h1></body>"
            "</html>",
            name);
-  rtb_response_set_content(resp, res);
+  rtb_response_set_content(resp, content);
 }
