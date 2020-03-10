efe3dd3649a4d77128ded091a48cd654b42bead3
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-10 22:43:30 +0300

    Delete calculate.js

--- calculate.js
+++
@@ -1,7 +0,0 @@
-function calculate() {
-    let x = Number(document.getElementById("x").value);
-    let y = Number(document.getElementById("y").value);
-    let z = Number(document.getElementById("z").value);
-    let ans = (x + y + z) /3;
-    document.getElementById("ans").innerHTML = ans;
-}
