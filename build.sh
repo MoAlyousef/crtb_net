1adfb124441092e6b59acb4ba62ff4b72444bc43
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-10 23:05:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ cmake --build bin
 # g++ -std=c++17 -DWIN32_LEAN_AND_MEAN -c -Iinclude -o bin/crtb_net.o src/rtb_net.cpp -fPIC
 # g++ -shared bin/crtb_net.o -lws2_32 -lwsock32 -o bin/libcrtb_net.so
 # gcc -o bin/server example.c bin/libcrtb_net.so
-# ./server.exe
+# ./bin/server.exe
 
 # clang++ -std=c++17 -c -Iinclude -o bin/crtb_net.o src/crtb_net.cpp -fPIC
 # clang++ -shared bin/crtb_net.o -o bin/libcrtb_net.so
 # clang -o bin/server example.c bin/libcrtb_net.so
-# ./server
+# ./bin/server
