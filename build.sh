7737100b866ab34801339c697ee3fc4bb8f61164
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-10 23:02:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ cmake --build bin
 
 # or uncommment below and change accordingly
 
-# g++ -std=c++17 -DWIN32_LEAN_AND_MEAN -c -I ../rtb/include rtb_server.cpp -fPIC
-# g++ -shared rtb_server.o -lws2_32 -lwsock32 -o librtb_server.so
-# gcc -o server example.c librtb_server.so
+# g++ -std=c++17 -DWIN32_LEAN_AND_MEAN -c -Iinclude -o bin/crtb_net.o src/rtb_net.cpp -fPIC
+# g++ -shared bin/crtb_net.o -lws2_32 -lwsock32 -o bin/libcrtb_net.so
+# gcc -o bin/server example.c bin/libcrtb_net.so
 # ./server.exe
 
-# clang++ -std=c++17 -c -I ../rtb/include rtb_server.cpp -fPIC
-# clang++ -shared rtb_server.o -o librtb_server.so
-# clang -o server example.c librtb_server.so
+# clang++ -std=c++17 -c -Iinclude -o bin/crtb_net.o src/crtb_net.cpp -fPIC
+# clang++ -shared bin/crtb_net.o -o bin/libcrtb_net.so
+# clang -o bin/server example.c bin/libcrtb_net.so
 # ./server
