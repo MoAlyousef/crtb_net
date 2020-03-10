d6de369a6312f7dd45b17fa47979065df734f2f7
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-10 19:37:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ cmake --build bin
 # g++ -std=c++17 -DWIN32_LEAN_AND_MEAN -c -I ../rtb/include rtb_server.cpp -fPIC
 # g++ -shared rtb_server.o -lws2_32 -lwsock32 -o librtb_server.so
 # gcc -o server main.c librtb_server.so
-
+# ./server.exe
 
 # clang++ -std=c++17 -c -I ../rtb/include rtb_server.cpp -fPIC
 # clang++ -shared rtb_server.o -o librtb_server.so
 # clang -o server main.c librtb_server.so
+# ./server
