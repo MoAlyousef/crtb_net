2990387bbfa3192d0f7b69f3294f0aff95010f25
Mohammed Alyousef <may642_2000@hotmail.com>
2020-03-15 00:16:57 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,19 +8,19 @@ int main(int argc, char **argv) {
 
     if(argc < 3) {
         fprintf(stderr, "Usage: ./"main host port");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     port = atol(argv[2]);
     if(!port) {
         fprintf(stderr, "Please enter a valid port");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     server = rtb_server_init(argv[1], port, ".", 2);
     if(!server) {
-        fprint(stderr, "Failed to initialize server");
-        return -1;
+        fprintf(stderr, "Failed to initialize server");
+        exit(EXIT_FAILURE);
     }
     return rtb_server_run(server);
 }
